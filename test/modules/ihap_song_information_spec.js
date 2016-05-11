8e925e02c78009a033ce9b59c45eef67df20ad3d
Carsten König <carstenkoenig92@gmail.com>
2016-05-11 15:44:02 +0200

    adds song info spec

---
+++ test/modules/ihap_song_information_spec.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect
+var jsdom = require('mocha-jsdom')
+var ihapSongInformation = require('../../src/js/modules/ihap_song_information.js')
+
+describe('ihap song_information module', function() {
+  // create dom
+  jsdom()
+
+  context('constructor', function() {
+    it('has no empty properties', function() {
+      let song_information = new ihapSongInformation();
+
+      expect(song_information.markup).not.to.be.null
+      expect(song_information.element).not.to.be.null
+    })
+
+    it('has correct properties', function() {
+      let song_information = new ihapSongInformation();
+
+      expect(song_information.markup.outerHTML).to.match(/<div\s*id="\w*"\s*class="\w*">.*<\/div>/g)
+      expect(song_information.element.outerHTML).to.match(/<div\s*id="\w*"\s*class="\w*">.*<\/div>/g)
+    })
+  })
+})
